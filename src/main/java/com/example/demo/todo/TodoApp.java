3981249f09c24d5e565dd97d01b60c5524dd5c66
Ilya Yahnis <ilavoroev@gmail.com>
2026-05-22 10:14:40 +0300

    create scene

--- src/main/java/com/example/demo/todo/TodoApp.java
+++ src/main/java/com/example/demo/todo/TodoApp.java
@@ -1,6 +1,12 @@
 package com.example.demo.todo;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -8,6 +14,29 @@ import java.io.IOException;
 public class TodoApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        BorderPane root = new BorderPane();
+        HBox buttonContainer = new HBox(20);
 
+
+        Button addTask = new Button("Добавить задачу");
+
+        buttonContainer.getChildren().addAll(addTask);
+
+
+        root.setBottom(buttonContainer);
+
+
+
+
+
+
+
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setScene(scene);
+        stage.show();
     }
+
+
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module com.example.demo {
 
 
     opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
     exports com.example.demo.todo;
     opens com.example.demo.todo to javafx.fxml;
 }
