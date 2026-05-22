d2cebf44c7020681bc3268106f790ad334039b23
Ilya Yahnis <ilavoroev@gmail.com>
2026-05-22 10:53:49 +0300

    First task info

--- src/main/java/com/example/demo/todo/Task.java
+++ src/main/java/com/example/demo/todo/Task.java
@@ -1,4 +1,14 @@
 package com.example.demo.todo;
 
 public class Task {
+    String name;
+    String description;
+    Boolean isCompleted;
+
+    public Task(String name, String description){
+        this.name = name;
+        this.description = description;
+        isCompleted = false;
+    }
+
 }
--- src/main/java/com/example/demo/todo/TodoApp.java
+++ src/main/java/com/example/demo/todo/TodoApp.java
@@ -1,12 +1,12 @@
 package com.example.demo.todo;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,12 +15,19 @@ public class TodoApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-color: black");
+
         HBox buttonContainer = new HBox(20);
 
-
         Button addTask = new Button("Добавить задачу");
+        addTask.setOnAction(event -> {
+            //всплывает окошко
+        });
+
+
 
         buttonContainer.getChildren().addAll(addTask);
+        buttonContainer.setStyle("-fx-padding: 20px; -fx-background-color: lightgray;");
 
 
         root.setBottom(buttonContainer);
@@ -32,11 +39,9 @@ public class TodoApp extends Application {
 
 
         Scene scene = new Scene(root, 1000, 600);
+
         stage.setScene(scene);
         stage.show();
     }
-
-
-
 
 }
