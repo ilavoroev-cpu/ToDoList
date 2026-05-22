b13c2189e8759f01b057c25813571ee2e6031543
Ilya Yahnis <ilavoroev@gmail.com>
2026-05-22 09:37:32 +0300

    Start ToDoList

---
+++ src/main/java/com/example/demo/todo/TodoApp.java
@@ -0,0 +1,13 @@
+package com.example.demo.todo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TodoApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+}
---
+++ src/main/java/com/example/demo/todo/TodoLogic.java
@@ -0,0 +1,4 @@
+package com.example.demo.todo;
+
+public class TodoLogic {
+}
